3217971f89412d9eaa49a25b4e934ad977dcc3a5
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-08-19 08:25:32 +0600

    "Capitalized 'String' in echo statement"

--- string.php
+++ string.php
@@ -24,5 +24,5 @@ $strings = ["Hello", "World", "PHP", "Programming"];
 foreach ($strings as $str) {
     $reversed = reverseStr($str);
     $vowels = vowelCount($str);
-    echo "Original string: $str, Vowel Count: $vowels, Reversed string: $reversed\n\n";
+    echo "Original String: $str, Vowel Count: $vowels, Reversed String: $reversed\n\n";
 }
